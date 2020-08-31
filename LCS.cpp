fc9d7b0c697966ed8d291ec3542de79148fdfb1f
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-08-31 13:53:11 +0530

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -1,20 +1,28 @@
+/*
+Least Common Subsequence
+*/
 #include<bits/stdc++.h>
 using namespace std;
-int LCSLength(string X,string Y,int m,int n){
-    if(m==0 || n==0){
-        return 0;
-    }
-    if(X[m-1]==Y[n-1]){
-        return(LCSLength(X,Y,m-1,n-1)+1);
-    }
-    return(max(LCSLength(X,Y,m,n-1),LCSLength(X,Y,m-1,n)));
-}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    string X;
-    string Y;
-    cin>>X>>Y;
-    cout<<LCSLength(X,Y,X.length(),Y.length());
+    string x,y;
+    cin>>x>>y;
+    int n=x.length();
+    int m=y.length();
+    int dp[n+1][m+1];
+    memset(dp,0,sizeof(dp[0][0])*(n+1)*(m+1));
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=m;j++){
+            if(x[i-1]==y[j-1]){
+                dp[i][j]=dp[i-1][j-1]+1;
+            }
+            else{
+                dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+    cout<<dp[n][m];
     return 0;
 }
